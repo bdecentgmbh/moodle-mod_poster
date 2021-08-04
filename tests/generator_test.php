39947b2a107c35d66fd3ab2f9e747305bdd6cc12
David Mudrák <david@moodle.com>
2021-08-04 14:33:42 +0200

    Add a simple activity data generator

---
+++ tests/generator_test.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Poster module generator tests.
+ *
+ * @package     mod_poster
+ * @category    test
+ * @copyright   2021 David Mudrák <david@moodle.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_poster_generator_test extends advanced_testcase {
+
+    /**
+     * Test {@see mod_poster_generator::create_instance()}.
+     */
+    public function test_create_instance() {
+        global $DB;
+
+        $this->resetAfterTest();
+        $this->setAdminUser();
+
+        $course = $this->getDataGenerator()->create_course();
+
+        $params = [
+            'course' => $course->id,
+            'name' => 'Test poster!',
+        ];
+
+        $this->assertFalse($DB->record_exists('poster', $params));
+
+        $poster = $this->getDataGenerator()->create_module('poster', $params);
+
+        $this->assertEquals(1, $DB->count_records('poster', $params));
+    }
+}
