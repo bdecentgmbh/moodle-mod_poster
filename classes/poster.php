7b78231b547b917bca9c42ff9723a91ae1936163
Stefan-Alexander Scholz <hello@bdecent.de>
2023-12-01 11:30:04 +0100

    Make code checker happy

--- classes/poster.php
+++ classes/poster.php
@@ -18,8 +18,8 @@
  * Define poster class.
  * @package mod_poster
  * @copyright bdecent gmbh 2023 <info@bdecent.de>
- *             based on the work by 
- *                 2015 David Mudrak <david@moodle.com> and 
+ *             based on the work by
+ *                 2015 David Mudrak <david@moodle.com> and
  *                 2019 Harald, David und Nicklas @devcamp19
  *
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
